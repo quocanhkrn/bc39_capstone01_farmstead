ef7c7ce7f768f3a268b74c275070bb97f74cae1c
DANG QUOC ANH <quocanh.krongno@gmail.com>
2022-10-13 10:15:10 +0700

    fix and minimize code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
-
-
-function onclick(){
+function onclick() {
     document.getElementById("feature-col").style.border = "1px solid #0000FF";
 };
+
+function toggleHamburgerMenu() {
+    var x = document.getElementById("hamburger_menu_toggle");
+    if (x.style.height === "384px") {
+        x.style.height = 0;
+    } else {
+        x.style.height = "384px";
+    }
+}
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("#navbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+    $(document).scroll(function () {
+        var $header = $("#header");
+        $header.toggleClass("scrolled", $(this).scrollTop() > $header.height());
+    });
+});
