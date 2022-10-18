05861646f76c806b74bbd223b2ca733249767d9f
DANG QUOC ANH <quocanh.krongno@gmail.com>
2022-10-18 21:06:30 +0700

    add back to top button

--- js/main.js
+++ js/main.js
@@ -21,3 +21,13 @@ $(function () {
         $header.toggleClass("scrolled", $(this).scrollTop() > $header.height());
     });
 });
+
+const toTop = document.querySelector(".back_to_top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
